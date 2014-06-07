97ece708ed14b18481ba75f4be072d55cb91f35e
hammerton <monkey_man2k@hotmail.com>
2014-06-06 23:26:28 -0400

    Another Commit

--- CheckForNewPoolSideTracks.py
+++ CheckForNewPoolSideTracks.py
@@ -3,26 +3,23 @@ import urllib
 import ast
 import soundcloud
 import sys
-import curses
+import os
 from threading import Thread
+
 
 def get_all_tracks():
     response = urllib2.urlopen("http://poolsideapi2.herokuapp.com/tracks")
     tracks = ast.literal_eval(response.read())
     return tracks
 
+
 def download_song(stream_url, song_name):
     url = urllib.urlopen(stream_url)
     # print url.getcode()
     if url.getcode() == 200:
-        track_dir = "/PoolSideTracks/%s" % song_name + ".mp3"
+        track_dir = "PoolSideTracks\%s" % song_name + ".mp3"
         urllib.urlretrieve(stream_url, track_dir)
 
-def update_progress(progress, cur_num, total_num, dataString):
-    stdscr = curses.initscr()
-    stdscr.clear()
-    sys.stdout.write('\rProcessed: {0}/{1} -- [{2}] {3}% ({4})'.format(cur_num, total_num, '#'*(progress/10), progress, dataString))
-    sys.stdout.flush()
 
 def download_track_list(track_list):
 
@@ -33,17 +30,18 @@ def download_track_list(track_list):
 
     for track in track_list:
         try:
+            song_name = "%s - %s" % (track['title'], track['artist'])
+            print "Downloading: %d/%d -- %s" %(track_no, num_of_tracks, song_name)
             the_track = client.get('/tracks/%s' % track['scId'])
             stream_url = client.get(the_track.stream_url, allow_redirects=False)
-            song_name = "%s - %s" % (track['title'], track['artist'])
             thread = Thread(target=download_song, args=[stream_url.location, song_name])
             thread.start()
             thread.join()
-            update_progress(int((float(track_no)/num_of_tracks)*100), track_no, num_of_tracks, song_name)
         except:
             pass
 
         track_no += 1
+
 
 def get_tracks_to_download(track_diff, get_tracks):
     tracks_to_download = []
@@ -52,14 +50,12 @@ def get_tracks_to_download(track_diff, get_tracks):
             tracks_to_download.append(track)
     return tracks_to_download
 
-def main():
 
-    f = open('poolSideTrackList.txt', 'r')
-    curr_tracks = [track.strip("\n.mp3") for track in f.readlines()]
+def main():
+    curr_tracks = [track.strip("\n.mp3") for track in os.listdir('C:\Users\Gary\Music\Poolside.fm')]
     get_tracks = get_all_tracks()
     new_tracks = [track["title"].strip() + " - " + track["artist"].strip() for track in get_tracks]
     track_diff = list(set(new_tracks) - set(curr_tracks))
-    f.close()
 
     tracks_to_download = get_tracks_to_download(track_diff, get_tracks)
     download_track_list(tracks_to_download)
